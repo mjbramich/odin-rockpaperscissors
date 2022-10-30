be8daeda2f99e846a6de4ad3f460e11739beab82
Matthew Bramich <matt_bramich@hotmail.com>
2022-10-30 20:19:45 +1100

    created external js file

---
+++ rps.js
@@ -0,0 +1,19 @@
+const random = 
+  Math.floor(Math.random()*3) + 1;
+
+
+
+
+const getComputerChoice = 
+  if(random === 1); {
+    console.log('Rock');
+  }elseIf ( random === 2); {
+    console.log('Scissors')
+  }elseIf ( random === 3 ); {
+    console.log ('Paper')
+  }
+  
+
+
+
+console.log(getComputerChoice)
