8bb19e2718c3d0c246e8bb647b629b60b3e69360
Matthew Bramich <matt_bramich@hotmail.com>
2022-11-02 16:54:33 +1100

    added scoring system
    tested in other js file and imported here

--- rps.js
+++ rps.js
@@ -7,27 +7,38 @@ const getComputerChoice = function() {
 };
 
 const computerSelection = getComputerChoice();
-const player= 'SCISSORS';
+const player = prompt('Please enter selection: ');
 const playerSelection = player.toLowerCase();
+let playerScore =0;
+let computerScore = 0;
+let score = `${playerScore} - ${computerScore}`;
 
 // play round function
 
-const playRound = function(playerSelection, computerSelection) {
-  if ((playerSelection === 'rock' && computerSelection === 'scissors') ||   
+const playRound = function(playerSelection, computerSelection) { 
+  
+  // win condition
+  if((playerSelection === 'rock' && computerSelection === 'scissors') ||
       (playerSelection === 'scissors' && computerSelection === 'paper') || 
       (playerSelection === 'paper' && computerSelection === 'rock')) {
-      console.log('You win, great job');
-}   else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
-    (playerSelection === 'scissors' && computerSelection === 'rock') ||
-    (playerSelection === 'paper' && computerSelection === 'scissors')) {
-      console.log ('You lose, better luck next time..');
-}   else if  (playerSelection === computerSelection) {
-    console.log ('It\'s a draw!');
+      console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+      playerScore++;
+
+  // lose condition
+}     else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
+      (playerSelection === 'scissors' && computerSelection === 'rock') ||
+      (playerSelection === 'paper' && computerSelection === 'scissors')) {
+      console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+      computerScore++;
+
+  // draw condition
+}     else if  (playerSelection === computerSelection) {
+      console.log('It\'s a draw!');
 } 
-
+    return score ;
 };
+playRound(playerSelection,computerSelection);
+// console.log(score);
 
-playRound(playerSelection, computerSelection);
-
-
+// game function
 
